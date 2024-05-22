e0b97b2befad3e95b2f911ba043c257f2b8c97e0
Heriberto Garcia <heribertogarcialeon@gmail.com>
2024-05-22 15:28:26 -0600

    refactor(answer-1): improve data handling and UI

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, WritableSignal } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { City } from '../../model/city.model';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemRefDirective } from '../../ui/list-item/list-item-ref.directive';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card [list]="cities()" (add)="addCity()">
+      <img src="assets/img/city.png" width="200px" />
+      <ng-template listItemRef let-item>
+        <app-list-item (delete)="deleteCity(item.id)">
+          {{ item.name }}
+        </app-list-item>
+      </ng-template>
+    </app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent, ListItemComponent, ListItemRefDirective],
+  styles: [
+    `
+      app-card {
+        background-color: rgba(0, 0, 250, 0.1);
+      }
+    `,
+  ],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cities: WritableSignal<City[]> = this.store.cities;
 
-  ngOnInit(): void {}
+  constructor(
+    private http: FakeHttpService,
+    private store: CityStore,
+  ) {}
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+  }
+
+  addCity() {
+    this.store.addOne(randomCity());
+  }
+
+  deleteCity(id: number) {
+    this.store.deleteOne(id);
+  }
 }
--- apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,31 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import { Component, OnInit, WritableSignal } from '@angular/core';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemRefDirective } from '../../ui/list-item/list-item-ref.directive';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
   template: `
-    <app-card
-      [list]="teachers"
-      [type]="cardType"
-      customClass="bg-light-red"></app-card>
+    <app-card [list]="teachers()" (add)="addTeacher()">
+      <img src="assets/img/teacher.png" width="200px" />
+      <ng-template listItemRef let-item>
+        <app-list-item (delete)="deleteTeacher(item.id)">
+          {{ item.firstName }} {{ item.lastName }}
+        </app-list-item>
+      </ng-template>
+    </app-card>
   `,
   styles: [
     `
-      ::ng-deep .bg-light-red {
+      app-card {
         background-color: rgba(250, 0, 0, 0.1);
       }
     `,
   ],
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, ListItemComponent, ListItemRefDirective],
 })
 export class TeacherCardComponent implements OnInit {
-  teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
+  teachers: WritableSignal<Teacher[]> = this.store.teachers;
 
   constructor(
     private http: FakeHttpService,
@@ -34,7 +41,13 @@ export class TeacherCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
+  }
 
-    this.store.teachers$.subscribe((t) => (this.teachers = t));
+  addTeacher() {
+    this.store.addOne(randTeacher());
+  }
+
+  deleteTeacher(id: number) {
+    this.store.deleteOne(id);
   }
 }
